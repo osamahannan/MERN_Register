0fae694b2e1d076ff2f37179076588b9c6f9d9f5
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-08-08 10:58:07 +0530

    connected our backend to database

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,32 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
+const DB= 'mongodb+srv://carnage:jarvis786@cluster0.7nxgl.mongodb.net/mernstack?retryWrites=true&w=majority'
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('connection successful');
+}).catch((e) => {
+    console.log('no connection')
+});
+
+// middelware
+
+const middelware = (req, res, next) => {
+    console.log('hello my middelware')
+    next();
+}
 
 app.get('/', (req, res)=> {
     res.send("This is Home Page");
 })
 
-app.get('/about', (req, res)=> {
+app.get('/about', middelware, (req, res)=> {
     res.send("This is About Page");
 })
 
