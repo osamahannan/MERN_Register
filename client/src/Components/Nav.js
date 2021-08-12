a0a416a8ccef88a0f414fc79e2923de9da5aad65
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-08-12 11:01:42 +0530

    created components and updated react-router-dom

---
+++ client/src/Components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div className= "nav-bar">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>SignIn</li>
+                <li>Register</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav
