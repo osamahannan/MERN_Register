5df2dac643909fb0ec5c01826caa09d6a0b3dcbe
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-08-12 11:00:48 +0530

    created components and updated react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-
+import './index.css';
+import Nav from "./Components/Nav";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+import About from "./Components/About";
+import Register from "./Components/Register";
+import Signin from "./Components/Signin";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello osama hannan</h1>
+      <>
+        <Nav/>
+      </>
     </div>
   );
 }
