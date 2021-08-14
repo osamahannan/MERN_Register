5dd09cf7e524a64c4546af57278b42db4dbdfea9
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-08-14 11:03:04 +0530

    updated contact page UI

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -2,8 +2,39 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact Page</h1>
+        <div className= "contact-main">
+            
+            <div className="saved-detail">
+
+                <div className="phone-info">
+                    <img src="#" alt="phone pic" />
+
+                    <div className="phone">
+                        <span>Phone</span>
+                        <span>+91 6306020618</span>
+                    </div>
+                </div>
+                <div className="email-info">
+                    <img src="#" alt="email pic" />
+
+                    <div className="email">
+                        <span>Email</span>
+                        <span>osamahannan9@gmail.com</span>
+                    </div>
+                </div>
+                <div className="address-info">
+                    <img src="#" alt="address pic" />
+
+                    <div className="address">
+                        <span>Address</span>
+                        <span>Lko, UP, India</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="contact-form">
+                contact form here !
+            </div>
         </div>
     )
 }
