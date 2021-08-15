016c4dbdb4c09c360e8a490ae7941676f1f00224
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-08-15 13:52:41 +0530

    updated About page UI

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import profilepic from '../assets/profile.jpg';
 
 const About = () => {
     return (
-        <div>
-           <h1>About Page</h1> 
+        <div className="about">
+
+            <div className="about-link">
+                <img src={profilepic} alt="profile pic" />
+
+                <div className="form-group">
+                    <a href="#">Linked In</a>
+                    <a href="#">Github</a>
+                    <a href="#">Instagram</a>
+                    <a href="#">Facebook</a>
+                    <a href="#">Twitter</a>
+                </div>
+            </div>
+
+            <div className="about-details">
+
+            </div>
         </div>
     )
 }
