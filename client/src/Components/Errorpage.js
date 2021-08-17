4539a367e7da0deebd665c98a0ab189246e9bbfb
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-08-17 20:55:32 +0530

    Error page UI completed

--- client/src/Components/Errorpage.js
+++ client/src/Components/Errorpage.js
@@ -11,9 +11,9 @@ const Errorpage = () => {
                 <div className="error-info">
                     <h2>WE ARE SORRY, PAGE NOT FOUND</h2>
                     <p>
-                        The page you are looking for might have beeb removed or had its name changed or its temporarily unavailable.
+                        The page you are looking for might have been removed or had its name changed or its temporarily unavailable.
                     </p>
-                    <Link to= "/" className= "back-button">BACK TO HOME</Link>
+                    <button><Link to= "/" className= "back-button">BACK TO HOME</Link></button>
                 </div>
                 
             </div>
