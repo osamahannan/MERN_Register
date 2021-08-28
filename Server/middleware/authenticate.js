ebfa224c850c33abfe3bfae8c087fdeef40c69cf
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-08-28 21:41:12 +0530

    updated about page and verified user authentication

--- Server/middleware/authenticate.js
+++ Server/middleware/authenticate.js
@@ -3,10 +3,20 @@ const User = require('../model/userSchema');
 
 const Authenticate = async (req, res, next) => {
     try {
+        const token = req.cookies.jwtoken;
+        const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
 
+        const rootUser = await User.findOne({ _id: verifyToken._id, "tokens.token": token });
+
+        if(!rootUser) {
+            throw new Error("User not found")
+        }
+
+        req.token = token
     } catch (err) {
-        res.status(401).send('')
+        res.status(401).send('Unauthorized: No token provided');
+        console.log(err);
     }
 }
 
-modeule.exports = Authenticate;
+module.exports = Authenticate;
--- Server/router/auth.js
+++ Server/router/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const authenticate = require("../middleware/authenticate");
 
 
 require('../db/conn');
