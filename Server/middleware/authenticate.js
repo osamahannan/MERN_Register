91d2588f9323691642d77ade2fe6e4a4073e5e56
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-08-28 18:55:49 +0530

    updated about page

---
+++ Server/middleware/authenticate.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+const User = require('../model/userSchema');
+
+const Authenticate = async (req, res, next) => {
+    try {
+
+    } catch (err) {
+        res.status(401).send('')
+    }
+}
+
+modeule.exports = Authenticate;
