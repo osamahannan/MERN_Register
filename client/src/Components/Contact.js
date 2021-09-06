f3ccb2188a279cdbb212ba1722b6611004f73d60
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-09-06 19:14:33 +0530

    updated message field of contact form using react hooks

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -1,11 +1,13 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import mobilepic from '../assets/mobile.png';
 import emailpic from '../assets/email.png';
 import addresspic from '../assets/address.png';
 
 const Contact = () => {
 
-    const [userData, setUserData] = useState({});
+    const [userData, setUserData] = useState({
+        name: "", email: "", phone: "", message: ""
+    });
 
     useEffect(() => {
         const callContactPage = async () => {
@@ -18,7 +20,7 @@ const Contact = () => {
                 });
 
                 const data = await res.json();
-                setUserData(data);
+                setUserData({ ...userData, name: data.name, email: data.email, phone: data.phone });
 
                 if (!res.status === 200) {
                     const error = new Error(res.error);
@@ -30,7 +32,13 @@ const Contact = () => {
             }
         }
         callContactPage();
-    }, []);
+    }, [userData]);
+
+    const handleInputs = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setUserData({ ...userData, [name]: value })
+    }
 
     return (
         <div className="contact-main">
@@ -38,7 +46,7 @@ const Contact = () => {
             <div className="saved-detail">
 
                 <div className="phone-info">
-                    <img src={mobilepic} className= "contact-pic" alt="phone pic" />
+                    <img src={mobilepic} className="contact-pic" alt="phone pic" />
 
                     <div className="phone">
                         <span>Phone</span>
@@ -46,7 +54,7 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="email-info">
-                    <img src={emailpic} className= "contact-pic" alt="email pic" />
+                    <img src={emailpic} className="contact-pic" alt="email pic" />
 
                     <div className="email">
                         <span>Email</span>
@@ -54,7 +62,7 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="address-info">
-                    <img src={addresspic} className= "contact-pic" alt="address pic" />
+                    <img src={addresspic} className="contact-pic" alt="address pic" />
 
                     <div className="address">
                         <span>Address</span>
@@ -72,22 +80,37 @@ const Contact = () => {
                 <div className="contact-detail">
 
                     <div className="contact-name">
-                        <input type="text" id="conatct-form-name" className="contact-form-name" placeholder="Your Name" required={true}
-                        value={userData.name}/>
+                        <input type="text" id="conatct-form-name" className="contact-form-name"
+                            name= "name"
+                            placeholder="Your Name" required={true}
+                            onChange={handleInputs}
+                            value={userData.name} />
                     </div>
 
                     <div className="contact-email">
-                        <input type="email" id="conatct-form-email" className="contact-form-email" placeholder="Your Email" required={true} value={userData.email}/>
+                        <input type="email" id="conatct-form-email" className="contact-form-email"
+                            name= "email"
+                            placeholder="Your Email" required={true}
+                            onChange={handleInputs}
+                            value={userData.email} />
                     </div>
 
                     <div className="contact-phone">
-                        <input type="number" id="conatct-form-phone" className="contact-form-phone" placeholder="Your Phone Number" required={true} value={userData.phone}/>
+                        <input type="number" id="conatct-form-phone" className="contact-form-phone"
+                            name= "phone"
+                            placeholder="Your Phone Number" required={true}
+                            onChange={handleInputs}
+                            value={userData.phone} />
                     </div>
 
                 </div>
 
                 <div className="contact-text">
-                    <textarea className="contact-text-field" placeholder="Message" cols="30" rows="10"></textarea>
+                    <textarea className="contact-text-field"
+                        name= "message"
+                        onChange={handleInputs}
+                        value={userData.message}
+                        placeholder="Message" cols="30" rows="10"></textarea>
                 </div>
 
                 <div className="contact-button">
