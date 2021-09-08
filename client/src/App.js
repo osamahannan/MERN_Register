d3eb02fcd7b9666990ff632280b4862b4b298cf0
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-09-08 21:26:14 +0530

    toggle login logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { createContext, useReducer} from 'react';
 import './index.css';
 import { Route, Switch } from 'react-router-dom';
 import Nav from "./Components/Nav";
@@ -8,45 +9,58 @@ import Signup from "./Components/Signup";
 import Login from "./Components/Login";
 import Errorpage from "./Components/Errorpage";
 import Logout from './Components/Logout';
+import { initialState, reducer } from '../src/reducer/UseReducer';
 
-function App() {
+//contextAPI
+export const UserContext = createContext();
+
+const Routing = () => {
   return (
-    <div className="App">
-      <>
-        <Nav />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-          <Route exact path="/About">
-            <About />
-          </Route>
+      <Route exact path="/About">
+        <About />
+      </Route>
 
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
+      <Route exact path="/contact">
+        <Contact />
+      </Route>
 
-          <Route exact path="/login">
-            <Login />
-          </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
 
-          <Route exact path="/signup">
-            <Signup />
-          </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
 
-          <Route exact path="/logout">
-            <Logout />
-          </Route>
+      <Route exact path="/logout">
+        <Logout />
+      </Route>
 
-          <Route>
-            <Errorpage />
-          </Route>
-        </Switch>
-
-      </>
-    </div>
+      <Route>
+        <Errorpage />
+      </Route>
+    </Switch>
   );
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <UserContext.Provider value={{ state, dispatch }}>
+
+        <Nav />
+        <Routing />
+
+      </UserContext.Provider>
+    </>
+  )
 }
 
 export default App;
