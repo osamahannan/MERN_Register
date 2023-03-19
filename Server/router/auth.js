54120ee08d00e35f028eac911653bee0d79e57c2
Osama Hannan <osamahannan7@gmail.com>
2023-03-19 16:06:09 +0530

    updated app.js in server

--- Server/router/auth.js
+++ Server/router/auth.js
@@ -10,77 +10,77 @@ require('../db/conn');
 router.use(cookieParser());
 const User = require('../model/userSchema');
 
-router.get('/', (req, res)=> {
+router.get('/', (req, res) => {
     res.send("This is Home Page from router js");
 })
 
 // Register
 router.post('/register', async (req, res) => {
 
-    const {name, email, phone, work, password, cpassword} = req.body;
+    const { name, email, phone, work, password, cpassword } = req.body;
 
-    if(!name || !email || !phone || !work || !password || !cpassword) {
-        return res.status(422).json({error: "please fill all the fields"})
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({ error: "please fill all the fields" })
     }
 
     try {
-        const userExist = await User.findOne({email:email})
+        const userExist = await User.findOne({ email: email })
 
-        
-        if(userExist) {
 
-            return res.status(409).json({error: "email already exist"})
+        if (userExist) {
 
-        } else if(password != cpassword) {
+            return res.status(409).json({ error: "email already exist" })
 
-            return res.status(422).json({error: "password not matched"})
+        } else if (password != cpassword) {
+
+            return res.status(422).json({ error: "password not matched" })
 
         } else {
 
-            const user = new User({name, email, phone, work, password, cpassword})
-    
+            const user = new User({ name, email, phone, work, password, cpassword })
+
             await user.save();
-            res.status(201).json({message: "user registerd successfully"})
+            res.status(201).json({ message: "user registered successfully" })
         }
 
     }
     catch (err) {
         console.log(err);
     }
-    
+
 })
 
 //signIn
 router.post('/signin', async (req, res) => {
-    
-    try {
-        const {email, password} = req.body;
 
-        if(!email || !password) {
-            return res.status(400).json({error: "please fill all the details"})
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(422).json({ error: "please fill all the details" })
         }
 
-        const userLogin = await User.findOne({email:email});
+        const userLogin = await User.findOne({ email: email });
 
-        if(userLogin) {
+        if (userLogin) {
 
             const isMatch = await bcrypt.compare(password, userLogin.password);
             const token = await userLogin.generateAuthToken();
             // console.log(token);
             res.cookie("jwtoken", token, {
-                expires:new Date(Date.now() + 25892000000),
+                expires: new Date(Date.now() + 25892000000),
                 httpOnly: true
             });
 
-            if(!isMatch) {
-    
-                res.status(400).json({error: "Invalid Credentials pass"})
-    
+            if (!isMatch) {
+
+                res.status(400).json({ error: "Invalid Credentials pass" })
+
             } else {
-                res.json({message: "user signIn successful"})
+                res.json({ message: "user signIn successful" })
             }
         } else {
-            res.status(400).json({error: "Ïnvalid Credentials"})
+            res.status(400).json({ error: "Ïnvalid Credentials" })
         }
 
     } catch (err) {
@@ -89,34 +89,34 @@ router.post('/signin', async (req, res) => {
 })
 
 // About Page
-router.get('/about', authenticate, async (req, res)=> {
+router.get('/about', authenticate, async (req, res) => {
     res.send(req.rootUser);
 })
 
-router.get('/getData', authenticate, async (req, res)=> {
+router.get('/getData', authenticate, async (req, res) => {
     res.send(req.rootUser);
 })
 
 // Contact page
-router.post('/contact', authenticate, async (req, res)=> {
-    
+router.post('/contact', authenticate, async (req, res) => {
+
     try {
 
-        const {name, email, phone, message } = req.body;
+        const { name, email, phone, message } = req.body;
 
-        if(!name || !email || !phone || !message) {
+        if (!name || !email || !phone || !message) {
             console.log("error in contact form");
-            return res.json({ error: "please fill the contact form"});
+            return res.json({ error: "please fill the contact form" });
         }
 
-        const userContact = await User.findOne({_id: req.userID})
+        const userContact = await User.findOne({ _id: req.userID })
 
-        if(userContact) {
+        if (userContact) {
 
             const userMessage = await userContact.addMessage(name, email, phone, message);
             await userContact.save();
 
-            res.status(201).json({ message: "User Contact Successful"});
+            res.status(201).json({ message: "User Contact Successful" });
         }
 
     } catch (error) {
@@ -125,8 +125,8 @@ router.post('/contact', authenticate, async (req, res)=> {
 })
 
 // Logout Page
-router.get('/logout', (req, res)=> {
-    res.clearCookie('jwtoken', { path: '/'});
+router.get('/logout', (req, res) => {
+    res.clearCookie('jwtoken', { path: '/' });
     res.status(200).send("User Logout Successful");
 })
 
