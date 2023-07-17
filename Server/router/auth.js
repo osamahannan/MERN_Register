24490a4592878e6d1c8ce974b15c73188dce70ff
Osama Hannan <osamahannan7@gmail.com>
2023-07-17 12:25:09 +0530

    added sameSite as none while logout auth.js

--- Server/router/auth.js
+++ Server/router/auth.js
@@ -141,7 +141,7 @@ router.post('/contact', authenticate, async (req, res) => {
 
 // Logout Page
 router.get('/logout', (req, res) => {
-    res.clearCookie('jwtoken', { path: '/' });
+    res.clearCookie('jwtoken', { path: '/' }, {sameSite: "none"});
     res.status(200).send("User Logout Successful");
 })
 
